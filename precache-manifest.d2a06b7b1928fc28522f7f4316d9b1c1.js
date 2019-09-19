0f5f487b1bb94340e5c94e152788b141099dcde2
nflaptop <55557931+rh-testcode@users.noreply.github.com>
2019-09-19 16:16:47 -0500

    Updates

---
+++ precache-manifest.d2a06b7b1928fc28522f7f4316d9b1c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577c480dd7e1f5a3477a96ef112fb25a",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "d439c020746342995331",
+    "url": "/hello-world/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "53dc7871265b0e046104",
+    "url": "/hello-world/static/js/2.d0fc553c.chunk.js"
+  },
+  {
+    "revision": "d439c020746342995331",
+    "url": "/hello-world/static/js/main.b6c2e612.chunk.js"
+  },
+  {
+    "revision": "165c18df8f25513f2b0f",
+    "url": "/hello-world/static/js/runtime-main.fb7e1377.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hello-world/static/media/logo.25bf045c.svg"
+  }
+]);
